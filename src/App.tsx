1e4fe9076856f7676678867dfaf47237fdad8b42
Leandro Pimentel <leandropimentel2011@gmail.com>
2021-05-19 17:49:28 -0300

    add modal and MirajeJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import  { useState } from "react";
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
-
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransacrionModalOpen, setIsNewTransacrionModalOpen] = useState(false);
+    function handleOpenNewTransactionModal() {
+        setIsNewTransacrionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransacrionModalOpen(false);
+    }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+
+      <Modal isOpen={isNewTransacrionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}>
+            <h2>Cadastrar nova transação</h2>
+        </Modal>
+
+
+
       <GlobalStyle />
     </>
   );
